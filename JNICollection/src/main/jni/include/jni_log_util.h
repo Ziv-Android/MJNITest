0b87080b76ca7a87ecfa7b6d842c9e80aecfb021
zhaopeng <Ziv_Android@Outlook.com>
2017-06-20 19:54:40 +0800

    update jni_log

--- JNICollection/src/main/jni/include/jni_log_util.h
+++ JNICollection/src/main/jni/include/jni_log_util.h
@@ -19,10 +19,10 @@
 #ifdef ALL_LOG
 #define LOG_TAG "ALL_LOG"
 #ifdef DEBUG
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __FILE__, __LINE__, __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __FILE__, __LINE__, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __FILE__, __LINE__, __VA_ARGS__)
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __FILE__, __LINE__, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 #else
 #define LOGI(...) do {} while (0)
 #define LOGW(...) do {} while (0)
@@ -33,7 +33,7 @@
 #else
 #ifdef DEBUG
 #define LOG_TAG "DEBUG"
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __FILE__, __LINE__, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 #else
 #define LOGI(...) do {} while (0)
 #define LOGW(...) do {} while (0)
--- Thread/src/main/java/com/ziv/thread/NativeThread.java
+++ Thread/src/main/java/com/ziv/thread/NativeThread.java
@@ -1,7 +1,7 @@
 package com.ziv.thread;
 
 /**
- * 原生线程相关
+ *
  *
  * Created by ziv on 17-6-20.
  */
